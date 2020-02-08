9571cbf105cd3fb4750b93dc66ac30945aa953bb
Salaheddin Badawi <salahbadawi12@gmail.com>
2020-02-08 14:50:19 +0200

    first assignment version

---
+++ SE v0/src/te/MainClass.java
@@ -0,0 +1,14 @@
+package te;
+
+public class MainClass {
+	public static void testMe(int versionNumber,int month) {
+		System.out.println("test code version number-> "+versionNumber+",month of the version-> "+month);
+	}
+
+	public static void main(String[] args) {
+		testMe(1,2);
+		// TODO Auto-generated method stub
+
+	}
+
+}
